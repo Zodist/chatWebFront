205b0901725b37c4aab8034e335267af6095d6ce
Zodist <zodist92@gmail.com>
2021-02-28 13:33:23 +0900

    socketIO주소변경

--- src/plugins/socketPlugin.js
+++ src/plugins/socketPlugin.js
@@ -1,8 +1,8 @@
 import Vue from 'vue';
 import io from 'socket.io-client';
 
-const socket = io('http://localhost:3001');
-
+//const socket = io('http://localhost:3001');
+const socket = io('http://ec2-3-84-249-206.compute-1.amazonaws.com:3001/char-room/hithere:3001');
 
 const SocketPlugin = {
   install(vue) {
